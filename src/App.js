764050c19224d89ee90b583377daf0bcce1b48fd
Suhaib Rajab <suhaibrajabo@gmail.com>
2024-01-19 20:10:04 +0300

    efef

--- src/App.js
+++ src/App.js
@@ -121,7 +121,7 @@ function App() {
         <div className="table-container">
           <h2>Data from Database</h2>
           <button className="refresh-button" onClick={fetchData}>
-            <FaSync /> Refresh
+            <FaSync />  Refresh
           </button>
           <div className="scrollable-table">
             <table>
